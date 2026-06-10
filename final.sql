713bf3e1640db078b81c79798a9de869120a0868
Vladislav K <110909601+ch1kim0n1@users.noreply.github.com>
2026-06-09 22:54:12 -0500

    Create final.sql

---
+++ final.sql
@@ -0,0 +1,16 @@
+SELECT
+    request_at AS Day,
+    ROUND(SUM(status != 'completed') / COUNT(*), 2) AS `Cancellation Rate`
+FROM Trips
+WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
+  AND client_id IN (
+      SELECT users_id
+      FROM Users
+      WHERE banned = 'No'
+  )
+  AND driver_id IN (
+      SELECT users_id
+      FROM Users
+      WHERE banned = 'No'
+  )
+GROUP BY request_at;
